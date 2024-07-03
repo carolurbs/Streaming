6838b5561975eababe988c16b9312b37d5f6cd7e
Caroline Urbainski <carolineurb.dev@outlook.com>
2024-07-03 18:02:53 -0300

    main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons= document.querySelectorAll('[data-tab-button]');
     const tabsContainer=document.querySelectorAll('[data-tab-id]')
-    const questions=document.querySelectorAll('[data-faq-question]'); //FAQ
     const heroSection= document.querySelector('.hero');
    const alturaHero= heroSection.clientHeight;
    window.addEventListener('scroll',function(){
@@ -13,11 +12,7 @@ document.addEventListener('DOMContentLoaded', function(){
     else{
         exibeElementosHeader();
     }
-
-
-
-
-   });
+});
     for(let i=0; i<buttons.length;i++){
         buttons[i].addEventListener('click', function(botao){
             const absAlvo=botao.target.dataset.tabButton;
@@ -29,19 +24,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
         })
     }
-/* For do faq */
-    for(let i=0;i<questions.length; i++){
-        questions[i].addEventListener('click',abreOuFechaResposta)
-
-    }
-
 })
-
-function abreOuFechaResposta(elemento){
-    const classe = 'faq__questions__item--is-open';
-    const elementoPai= elemento.target.parentNode;
-    elementoPai.classList.toggle(classe);
-}
 function removeBotaoAtivo(){
     const buttons= document.querySelectorAll('[data-tab-button]');
     for(let i=0; i<buttons.length;i++){
